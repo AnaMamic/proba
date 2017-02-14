d2ba3a5d09194b2aa00e73818953447a76a397bd
AnaMamic <amamic@pmfst.hr>
2017-02-14 16:31:36 +0100

    commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+let strin = String(format: "nesto %d hfshdj %d", 123, 12)
+let z = 5435
+let nesto = String(format : "http://adserver.iprom.net/adserver7/Click?z=%d;ad=140886;m=RTL;sid=vijesti_hr;ssid=novosti;flash=0;rdr=http%3A%2F%2Fwww%2Eiprom%2Eeu;kw=;tm=1484727217", z)
+let nesto2 = "http://adserver.iprom.net/adserver7/Click?z=\(z);ad=140886;m=RTL;sid=vijesti_hr;ssid=novosti;flash=0;rdr=http%3A%2F%2Fwww%2Eiprom%2Eeu;kw=;tm=1484727217"
+
+print(nesto)
